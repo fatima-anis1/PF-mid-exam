5e4606fcbf514c0950ccab68aa8f734e3d280588
fatma-anis1 <anisf8989@gmail.com>
2024-12-04 14:34:22 +0500

    D

---
+++ pf mids.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int income, expense,balance;
+	
+	cout<<"Enter income amount:"<<income<<"\n";
+	cin>>income;
+	
+	cout<<"Enter expense amount:"<<expense<<"\n";
+	cin>>expense;
+	
+	cout<<"Enter current balance:"<<current<<"\n";
+	cin>>current balance;
+	
+	switch(income){
+		case 1:
+			cout<<"Income amount is 0";
+			cin>>amount;
+			income +=balance;
+			cout<<total income;
+			break;
+			
+			case 2:
+				cout<<"Expense amount";
+				cin>>expense;
+				expense -=balance;
+				cout<<total expenses;
+				break;
+				
+				case 3;
+				cout<<"Display Current balance";
+				cin>>balance;
+				cout<<total income-= total expenses;
+				continue;
+	}
+	return0;
+}
